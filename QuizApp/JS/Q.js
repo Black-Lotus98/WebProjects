8f83b5699d3afd94833e9a2061326b612d801b63
Black-Lotus98 <Qusaifannoun@gmail.com>
2021-07-06 15:58:55 +0300

    add new data

--- QuizApp/JS/Q.js
+++ QuizApp/JS/Q.js
@@ -2,43 +2,63 @@ var $Timer = $("#timer");
 var $Content = $("#content");
 var $ButtonsCont = $("#buttonsCont");
 var RandomQuestions = [];
-var i;
 var aa=[];
+var time;
+var i;
 
 
-let time=20;
+function RandomQuestionsPool(Rand, num)
+{
+    while(Rand.length < 5)
+    {
+        var r = Math.floor(Math.random() * (num));
+        if(Rand.indexOf(r) === -1) 
+        {
+            Rand.push(r);
+        }
+    }
+}
+
+
+function getDataFromJsonFile()
+{
+    i=0
+    $.ajax({
+        type: "GET",
+        url: "../JSON/Questions.json",
+        data: "data",
+        dataType: "json",
+        success: function (questionsData) {
+            aa=questionsData
+            if(RandomQuestions.length==0)
+            {
+                RandomQuestionsPool(RandomQuestions,aa.length);
+            }
+        }
+    });
+}    
+
+
 
 
 function DisplayQuestions()
 {
-  
-  $.ajax({
-    type: "GET",
-    url: "../JSON/Questions.json",
-    data: "data",
-    dataType: "json",
-    success: function (questionsData) {
-      aa=questionsData;
-      if(RandomQuestions.length==0)
-      {
-        RandomQuestionsPool(RandomQuestions,aa.length);
-      }
-      $Content.empty();
-      $ButtonsCont.empty();
-      if(RandomQuestions[i]===undefined)
-      {
+    $Content.empty();
+    $ButtonsCont.empty();
+    if(RandomQuestions[i]===undefined)
+    {
         $Content.append(`Last Q`);
         $Timer.append(`Time`);
         $ButtonsCont.append(`<div class="d-flex col-6 justify-content-Start align-items-end"><button type="button" onclick="PreviousQuestion(`+RandomQuestions[i-1]+`)" class="btn btn-outline-light btn-lg btn-block">Previous</button></div>`);
-      }
-      else
-      {
+    }
+    else
+    {
         time=aa[RandomQuestions[i]].Time;
+        displayTime(aa[i].Time);
         console.log(`aa[RandomQuestions[i]].Time`, aa[RandomQuestions[i]].Time)
-        console.log(aa[RandomQuestions[i]])  
-
         
-          $Content.append(
+        
+        $Content.append(
             `
             <form action="">
             <h4 id="question">`+aa[RandomQuestions[i]].question+`</h4>
@@ -47,6 +67,7 @@ function DisplayQuestions()
             <label class="form-check-label" for="option1">
             A . `+aa[RandomQuestions[i]].Options.A+`
             </label>
+            
             </div>
             <div class="form-check">
             <input class="form-check-input" type="radio" name="exampleRadios" id="option2" value="option2">
@@ -70,89 +91,35 @@ function DisplayQuestions()
             <br>       
             `
             )
-            if(i>0)
-            {
-              $ButtonsCont.append(`<div class="d-flex col-6 justify-content-Start align-items-end"><button type="button" onclick="PreviousQuestion(`+RandomQuestions[i-1]+`)" class="btn btn-outline-light btn-lg btn-block">Previous</button></div>`);        
-            }
-            if(i<aa.length)
-            {
-              $ButtonsCont.append(`<div class="d-flex col-6 justify-content-end align-items-end"><button type="button" onclick="NextQuestion(`+RandomQuestions[i]+`)" class="btn btn-outline-light btn-lg btn-block">Next</button></div>`); 
-            }
+            
+        if(i>0)
+        {
+            $ButtonsCont.append(`<div class="d-flex col-6 justify-content-Start align-items-end"><button type="button" onclick="PreviousQuestion(`+RandomQuestions[i]+`)" class="btn btn-outline-light btn-lg btn-block">Previous</button></div>`);        
         }
-      }
-      });
-    }
-
-
-function RandomQuestionsPool(Rand, num)
-{
-  while(Rand.length < 5)
-  {
-    var r = Math.floor(Math.random() * (num));
-    if(Rand.indexOf(r) === -1) 
-    {
-      Rand.push(r);
-    }
+        if(i<aa.length)
+        {
+            $ButtonsCont.append(`<div class="d-flex col-6 justify-content-end align-items-end"><button type="button" onclick="NextQuestion(`+RandomQuestions[i]+`)" class="btn btn-outline-light btn-lg btn-block">Next</button></div>`); 
+        }
     }
 }
-
-function start()
-{
-  i=0;
-  DisplayQuestions();
-  displayTime(time);
-  startTimer();
-}
+       
 
 function startTimer()
 {
-  const countDown = setInterval(()=>{
-    time--;
-    displayTime(time);
-    if(time<=0 )
-    {
-        clearInterval(countDown);
-    }
     
+    setInterval(function(){
+        if(time > 0)
+        {
+            time--;
+            displayTime(time);
+        }
+        if(time<=0 )
+        {
+            return;
+        }     
     },1000)
-}
-
-
-
-
-function NextQuestion(index)
-{
-  if(aa[i]===undefined)
-  {
-    DisplayQuestions();
-  }
-  else
-  {
-    DisplayQuestions();
     
-    i++;
-    aa[index].Time=aa[index].Time-aa[index].TimeCounter;
-    aa[index].TimeCounter+= time- aa[index].Time;
-    console.log(`aa[index]`, aa[index])
-  }
 }
-function PreviousQuestion(index)
-{
-  if(aa[i]===undefined)
-  {
-    DisplayQuestions();
-  }
-  else
-  {
-    i--;
-    DisplayQuestions();
-    time=aa[index].Time;
-  }
-   
-}
-
-
-
 
 
 function displayTime(second)
@@ -162,3 +129,35 @@ function displayTime(second)
     $Timer.empty();
     $Timer.append(`Time Remaining ${min <10 ? '0' : ''}${min}:${sec<10 ? '0': ''}${sec}`);
 }
+
+
+function start()
+{
+  i=0;
+  DisplayQuestions();
+  aa[RandomQuestions[i]].Time=time;
+  startTimer();
+  
+}
+
+
+function NextQuestion(index)
+{
+    aa[index].Time=time;
+    i++;
+    DisplayQuestions();    
+}
+
+
+function PreviousQuestion(index)
+{
+    i--;
+    aa[index].Time=time;  
+    time=aa[index].Time;
+
+    DisplayQuestions();
+}
+
+
+
+getDataFromJsonFile();
